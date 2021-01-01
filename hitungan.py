44f9dd2ea1d71b256798848e7e408a4cc2a56add
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2021-01-01 22:21:37 +0700

    data orde 2

---
+++ hitungan.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+pi = np.matrix('2.14 0 0 0 0; 3.23 2.14 0 12.00 0;4.11 3.23 2.14 22.30 12.00;4.63 4.11 3.23 32.0 22.0;5.10 4.63 4.11 42.0 32.0')
+y= np.matrix('12;22;32;42;62')
+tetha = y = np.linalg.inv(pi.T * pi) * pi.T * y 
+
+
+if __name__ == '__main__' :
+    print("Data Orde 2")
+    print(tetha)
