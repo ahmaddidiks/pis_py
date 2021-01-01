d89799d7d6edc9bfc9613f3efd679908514d8f3c
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2021-01-01 22:30:09 +0700

    dataOrde2

--- hitungan.py
+++ hitungan.py
@@ -3,11 +3,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-pi = np.matrix('2.14 0 0 0 0; 3.23 2.14 0 12.00 0;4.11 3.23 2.14 22.30 12.00;4.63 4.11 3.23 32.0 22.0;5.10 4.63 4.11 42.0 32.0')
-y= np.matrix('12;22;32;42;62')
-tetha = np.linalg.inv(pi.T * pi) * pi.T * y 
+def dataOrde2():
+    pi = np.matrix('2.14 0 0 0 0; 3.23 2.14 0 12.00 0;4.11 3.23 2.14 22.30 12.00;4.63 4.11 3.23 32.0 22.0;5.10 4.63 4.11 42.0 32.0')
+    y= np.matrix('12;22;32;42;62')
+    global tetha
+    tetha = np.linalg.inv(pi.T * pi) * pi.T * y
+
+ 
 
 
 if __name__ == '__main__' :
+    dataOrde2()
     print("Data Orde 2")
     print(tetha)
