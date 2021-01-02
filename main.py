3b1bc22411efdc2b94d3cbab79d107b9c30da9de
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2021-01-02 18:05:23 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from data import *
+from func import *
+
+def percobaan1():
+    dataOrde1()
+    autoRegresiveOrde1()
+
+
+if __name__ == '__main__' :
+    percobaan1()
+    
