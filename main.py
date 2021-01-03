16c5dcc8e8913afd2e4f2e2e4d9a96d360aa4588
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2021-01-03 14:44:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ def percobaan2():
     dataOrde2()
     autoRegresiveOrde2()
 
+def percobaan3():
+    dataOrde1()
+    movingAverageOrde1()
+
+def percobaan4():
+    dataOrde2()
+    movingAverageOrde2()
+
+def percobaan5():
+    dataOrde1()
+    ARMAOrde1()
+
+def percobaan6():
+    dataOrde2()
+    ARMAOrde2()
 
 if __name__ == '__main__' :
-    percobaan2()
-    
+    percobaan6()
